acec5a74c7c773e423d0d942af63ab42d1e40fa5
Aadi <aadishchaubal@hotmail.com>
2024-12-20 11:23:17 -0500

    add support for wayland

--- code-nautilus.py
+++ code-nautilus.py
@@ -6,8 +6,7 @@
 # This script is released to the public domain.
 
 from gi.repository import Nautilus, GObject
-from subprocess import call
-import os
+import os, subprocess
 
 # path to vscode
 VSCODE = 'code'
@@ -15,29 +14,23 @@ VSCODE = 'code'
 # what name do you want to see in the context menu?
 VSCODENAME = 'Code'
 
-# always create new window?
-NEWWINDOW = False
-
-
 class VSCodeExtension(GObject.GObject, Nautilus.MenuProvider):
 
     def launch_vscode(self, menu, files):
         safepaths = ''
         args = ''
 
+        if os.environ.get('XDG_SESSION_TYPE') == "wayland": args = ' --ozone-platform-hint=auto'
+
         for file in files:
             filepath = file.get_location().get_path()
-            safepaths += '"' + filepath + '" '
-
+            safepaths += ' "' + filepath + '" '
             # If one of the files we are trying to open is a folder
             # create a new instance of vscode
             if os.path.isdir(filepath) and os.path.exists(filepath):
-                args = '--new-window '
+                args = ' --new-window' + args
 
-        if NEWWINDOW:
-            args = '--new-window '
-
-        call(VSCODE + ' ' + args + safepaths + '&', shell=True)
+        subprocess.run(VSCODE + args + safepaths + '&', shell=True)
 
     def get_file_items(self, *args):
         files = args[-1]
